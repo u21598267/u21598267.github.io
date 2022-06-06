659ff543e90a4a958afcb0ee67272bef609e8348
u21598267 <102956551+u21598267@users.noreply.github.com>
2022-06-06 08:53:42 +0200

    HW03
    The first upload of code from HW03 INF272. 90% Completed Home Page. Testing of file uploaded completed and working.

---
+++ u21598267_HW03/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="u21598267_HW03.MvcApplication" Language="C#" %>
---
+++ u21598267_HW03/Models/FileModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace u21598267_HW03.Models
+{
+    public class FileModel
+    {
+        public string FileName { get; set; }
+    }
+}
