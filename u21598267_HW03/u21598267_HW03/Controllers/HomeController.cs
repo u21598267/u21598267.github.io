c09090155b3a47a1a46362c2fad0112515935eeb
u21598267 <102956551+u21598267@users.noreply.github.com>
2022-06-06 23:40:30 +0200

    Started from scratch

---
+++ u21598267_HW03/u21598267_HW03/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+namespace u21598267_HW03.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()  
+        {
+            return View();
+        }
+
+      
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase files) 
+        {
+          
+
+            if (files != null && files.ContentLength > 0)
+            {
+             
+
+                var fileName = Path.GetFileName(files.FileName);
+
+              
+
+                var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
+              
+
+                files.SaveAs(path);
+
+            }
+
+            return RedirectToAction("Index");
+        }
+public ActionResult About()
+        {
+          
+
+            return View();
+        }
+
+       
+    }
+}
---
+++ u21598267_HW03/u21598267_HW03/Controllers/ImagesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace u21598267_HW03.Controllers
+{
+    public class ImagesController : Controller
+    {
+        // GET: Images
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
